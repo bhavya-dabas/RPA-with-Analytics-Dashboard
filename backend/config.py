9b7ea50bddac389086be0c897e74ba55060df68d
Bhavya Dabas <144929360+bhavya-dabas@users.noreply.github.com>
2025-07-17 14:31:09 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,4 @@
 import os
 
-DB_URL = os.environ.get("DB_URL", "sqlite:///../database/tickets.db")
+DB_URL = os.environ.get("DB_URL", "sqlite:///../database/tickets.db?check_same_thread=False&timeout=30")
 NOTIFY_EMAIL = "noc_manager@company.com"
